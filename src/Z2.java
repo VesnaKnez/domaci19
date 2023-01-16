2b4313925171297f8a658b722c77eb0d44b5c1f0
VesnaKnez <vesna__1997@hotmail.com>
2023-01-16 11:55:11 +0100

    izmena 2

--- src/Z2.java
+++ src/Z2.java
@@ -2,6 +2,7 @@ public class Z2 {
     public static void main(String[] args) {
         String tekst = "Mama ima momu.";
         System.out.println(prebrojiSlovo(tekst,'M'));
+        System.out.println(prebrojiSlovo(tekst,'s'));
 
     }
     public static int prebrojiSlovo (String tekst, char slovo) {
